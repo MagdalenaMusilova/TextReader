ea68afc529d8be741120b9651a8da8a8e0e3b7ad
musilmag <musilmag@cvut.cz>
2026-07-30 19:38:14 +0200

    Add scrolling

--- TextReaderControl.xaml.cs
+++ TextReaderControl.xaml.cs
@@ -16,6 +16,8 @@ public partial class TextReaderControl : UserControl
     private readonly DrawingVisual _drawingVisual;
     
     private readonly List<string> _lines = new();
+    private int _curLineCount = 0;
+    private int _linesShownCount = 15;
     
     public TextReaderControl()
     {
@@ -49,12 +51,27 @@ public partial class TextReaderControl : UserControl
     {
         using (DrawingContext dc = _drawingVisual.RenderOpen())
         {
-            for (int i = 0; i < _lines.Count; i++)
+            // lineI = index of which line is being printed, basically _curLineCount + i
+            // renderI = index of which line in the reader is being printed. 0 means at the top of the window
+            for (int lineI = _curLineCount, renderI = 0;
+                 renderI < _linesShownCount && lineI < _lines.Count;
+                 lineI++, renderI++)
             {
-                var formattedText = CreateFormattedText(_lines[i]);
-                double yPos = YOffset + i * _lineHeight;
+                var formattedText = CreateFormattedText(_lines[lineI]);
+                double yPos = YOffset + renderI * _lineHeight;
                 dc.DrawText(formattedText, new Point(XOffset, yPos));
             }
         }
+    }
+
+    private void Scroll_OnScroll(object sender, ScrollEventArgs e)
+    {
+        if ((int)ScrollBar.Value == _curLineCount)
+        {
+            return;
+        }
+
+        _curLineCount = (int)ScrollBar.Value;
+        RenderVisibleLines();
     }
 }
