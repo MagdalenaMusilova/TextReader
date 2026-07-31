57fd80c7f2f1be115051724b12b24e7ccf44b9ea
musilmag <musilmag@cvut.cz>
2026-07-31 20:04:42 +0200

    Add search function
    Not yet fully hooked up to work. Only the logic is present + visually.

---
+++ Models/WordPosition.cs
@@ -0,0 +1,7 @@
+﻿namespace TextReader.Models;
+
+public struct WordPosition
+{
+    public long lineIndex;
+    public long lineOffset;
+}
---
+++ Search/SearchFeature.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using TextReader.TextInputs;
+
+namespace TextReader;
+
+public class SearchFeature
+{
+    private Dictionary<string, List<int>> cache = new Dictionary<string, List<int>>();
+    private ITextInput _input;
+    
+    public SearchFeature(ITextInput input)
+    {
+        _input = input;
+    }
+    
+    public List<int> Search(string word)
+    {
+        if (cache.ContainsKey(word))
+        {
+            return cache[word];
+        }
+        
+        // try to help with the search from previous searches
+        for (int i = word.Length - 1; i >= 1; i--)
+        {
+            string subWord = word.Substring(0, i);
+            if (cache.ContainsKey(subWord))
+            {
+                return FindAllOccurencesFromPossibilities(word, cache[subWord]);
+            }
+        }
+        
+        return FindAllOccurencesRaw(word);
+    }
+    
+    private List<int> FindAllOccurencesFromPossibilities(string word, List<int> possibilities)
+    {
+        foreach (var index in possibilities)
+        {
+            _input.Seek(index);
+            if (IsWordAtPosition(_input, word))
+            {
+                cache[word].Add(index);
+            }
+        }
+        return cache[word];
+    }
+
+    private List<int> FindAllOccurencesRaw(string word)
+    {
+        cache[word] = new List<int>();
+        int index = 0;
+        while (!_input.EOF)
+        {
+            _input.Seek(index);
+            if (IsWordAtPosition(_input, word))
+            {
+                cache[word].Add(index);
+            }
+            index++;
+        }
+        return cache[word];
+    }
+
+    private bool IsWordAtPosition(ITextInput input, string word)
+    {
+        int readByte;
+        for (int i = 0; i < word.Length; i++)
+        {
+            readByte = _input.ReadByte();
+            if (readByte != word[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
